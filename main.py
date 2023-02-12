1c88c0a0b5a7cd1675a02313eaf8a6d64b1fa5be
Renat2492 <125185019+Renat2492@users.noreply.github.com>
2023-02-12 16:15:18 +0400

    Обновить main.ts, main.py

--- main.py
+++ main.py
@@ -2,26 +2,46 @@ signs = "+-*/"
 first_num = 0
 second_num = 0
 count_ab = 0
+signs_count = 0
 
 def on_button_a():
-    global first_num, second_num, count_ab
+    global first_num, second_num, count_ab, signs_count
     if count_ab == 0:
         first_num = first_num - 1
     if count_ab == 2:
-        second_num - 1
+        second_num -= 1
+    if count_ab == 1: 
+        signs_count = signs_count - 1 
 
 def on_button_b():
-    global first_num, second_num, count_ab
-    if count_ab:
+    global first_num, second_num, count_ab, signs_count
+    if count_ab == 0:
         first_num = first_num + 1
     if count_ab == 2:
-        second_num + 1
+        second_num += 1
+    if count_ab == 1:
+        signs_count == signs_count - 1 
         
+def change_mode():
+    global count_ab
+    count_ab += 1
+
 def calculate():
-    global first_num, second_num, count_ab
+    global first_num, second_num, count_ab, signs_count
     if count_ab == 0:
         input.on_button_pressed(Button.A, on_button_a)
         input.on_button_pressed(Button.B, on_button_b)
+        basic.show_number(first_num)
+        input.on_button_pressed(Button.AB, change_mode)
     elif count_ab == 2:
         input.on_button_pressed(Button.A, on_button_a)
         input.on_button_pressed(Button.B, on_button_b)
+        basic.show_number(second_num)
+        input.on_button_pressed(Button.AB, change_mode)
+    elif count_ab == 1:
+        input.on_button_pressed(Button.A, on_button_a)
+        input.on_button_pressed(Button.B, on_button_b)
+        basic.show_string(signs[signs_count%len(signs)])
+        input.on_button_pressed(Button.AB, change_mode)
+
+basic.forever(calculate)
--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ let signs = "+-*/"
 let first_num = 0
 let second_num = 0
 let count_ab = 0
+let signs_count = 0
 function on_button_a() {
     
     if (count_ab == 0) {
@@ -9,32 +10,53 @@ function on_button_a() {
     }
     
     if (count_ab == 2) {
-        second_num - 1
+        second_num -= 1
+    }
+    
+    if (count_ab == 1) {
+        signs_count = signs_count - 1
     }
     
 }
 
 function on_button_b() {
     
-    if (count_ab) {
+    if (count_ab == 0) {
         first_num = first_num + 1
     }
     
     if (count_ab == 2) {
-        second_num + 1
+        second_num += 1
+    }
+    
+    if (count_ab == 1) {
+        signs_count == signs_count - 1
     }
     
 }
 
-function calculate() {
+function change_mode() {
+    
+    count_ab += 1
+}
+
+basic.forever(function calculate() {
     
     if (count_ab == 0) {
         input.onButtonPressed(Button.A, on_button_a)
         input.onButtonPressed(Button.B, on_button_b)
+        basic.showNumber(first_num)
+        input.onButtonPressed(Button.AB, change_mode)
     } else if (count_ab == 2) {
         input.onButtonPressed(Button.A, on_button_a)
         input.onButtonPressed(Button.B, on_button_b)
+        basic.showNumber(second_num)
+        input.onButtonPressed(Button.AB, change_mode)
+    } else if (count_ab == 1) {
+        input.onButtonPressed(Button.A, on_button_a)
+        input.onButtonPressed(Button.B, on_button_b)
+        basic.showString(signs[signs_count % signs.length])
+        input.onButtonPressed(Button.AB, change_mode)
     }
     
-}
-
+})
